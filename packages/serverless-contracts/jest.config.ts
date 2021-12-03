26685fcabad6c24dab3af512dfe97f166725ffa3
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-12-03 15:58:38 +0100

    test: make tests work

--- packages/serverless-contracts/jest.config.ts
+++ packages/serverless-contracts/jest.config.ts
@@ -1,10 +1,13 @@
 import { resolve } from 'path';
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
 
 import { jestConfig } from '@serverless-contracts/configuration';
 
-export default {
+const config: InitialOptionsTsJest = {
   ...jestConfig,
   moduleNameMapper: {
     '^utils/(.*)$': resolve(__dirname, 'utils/$1'),
   },
 };
+
+export default config;
