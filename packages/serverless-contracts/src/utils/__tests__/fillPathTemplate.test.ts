e2b581583b4d0cad8d1eb81c243c050c60918fb2
fargito <fr.farge96@gmail.com>
2022-02-11 16:09:03 +0100

    chore: update contracts

--- packages/serverless-contracts/src/utils/__tests__/fillPathTemplate.test.ts
+++ packages/serverless-contracts/src/utils/__tests__/fillPathTemplate.test.ts
@@ -1,5 +1,10 @@
 import { fillPathTemplate } from '../fillPathTemplate';
 
+/**
+ * unit tests
+ *
+ * @group unit/utils
+ */
 describe('fillPathTemplate', () => {
   it('should not fail if no values are passed', () => {
     expect(fillPathTemplate('/hello')).toBe('/hello');
