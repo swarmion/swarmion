b45537d4fd905cc78e8e99616bdd0c8672984545
Guillaume Duboc <guillaumed@theodo.fr>
2022-04-22 17:13:41 +0200

    feat: remove unused code from index and create-app
    Co-authored-by: MaximeVivier <maxime.vivier@centrale-marseille.fr>

--- packages/create-swarmion-app/index.ts
+++ packages/create-swarmion-app/index.ts
@@ -6,8 +6,7 @@ import chalk from 'chalk';
 import Commander from 'commander';
 import path from 'path';
 import prompts from 'prompts';
-import checkForUpdate from 'update-check';
-import { createApp, DownloadError } from './create-app';
+import { createApp } from './create-app';
 import { getPkgManager } from './helpers/get-pkg-manager';
 import { validateNpmName } from './helpers/validate-pkg';
 import packageJson from './package.json';
@@ -17,50 +16,9 @@ let projectPath = '';
 const program = new Commander.Command(packageJson.name)
   .arguments('<project-directory>')
   .usage(`${chalk.green('<project-directory>')} [options]`)
-  .action(name => {
+  .action((name: string) => {
     projectPath = name;
   })
-  .option(
-    '--ts, --typescript',
-    `
-
-  Initialize as a TypeScript project.
-`,
-  )
-  .option(
-    '--use-npm',
-    `
-
-  Explicitly tell the CLI to bootstrap the app using npm
-`,
-  )
-  .option(
-    '--use-pnpm',
-    `
-
-  Explicitly tell the CLI to bootstrap the app using pnpm
-`,
-  )
-  .option(
-    '-e, --example [name]|[github-url]',
-    `
-
-  An example to bootstrap the app with. You can use an example name
-  from the official Next.js repo or a GitHub URL. The URL can use
-  any branch and/or subdirectory
-`,
-  )
-  .option(
-    '--example-path <path-to-example>',
-    `
-
-  In a rare case, your GitHub URL might contain a branch name with
-  a slash (e.g. bug/fix-1) and the path to the example (e.g. foo/bar).
-  In this case, you must specify the path to the example separately:
-  --example-path foo/bar
-`,
-  )
-  .allowUnknownOption()
   .parse(process.argv);
 
 const run = async (): Promise<void> => {
@@ -74,7 +32,7 @@ const run = async (): Promise<void> => {
       name: 'path',
       message: 'What is your project named?',
       initial: 'my-app',
-      validate: name => {
+      validate: (name: string) => {
         const validation = validateNpmName(path.basename(path.resolve(name)));
         if (validation.valid) {
           return true;
@@ -129,88 +87,29 @@ const run = async (): Promise<void> => {
     process.exit(1);
   }
 
-  const packageManager = program.useNpm
-    ? 'npm'
-    : program.usePnpm
-    ? 'pnpm'
-    : getPkgManager();
+  const packageManager = getPkgManager();
 
-  const example = typeof program.example === 'string' && program.example.trim();
-  try {
-    await createApp({
-      appPath: resolvedProjectPath,
-      packageManager,
-      example: example && example !== 'default' ? example : undefined,
-      examplePath: program.examplePath,
-      typescript: program.typescript,
-    });
-  } catch (reason) {
-    if (!(reason instanceof DownloadError)) {
-      throw reason;
-    }
-
-    const res = await prompts({
-      type: 'confirm',
-      name: 'builtin',
-      message:
-        `Could not download "${example}" because of a connectivity issue between your machine and GitHub.\n` +
-        `Do you want to use the default template instead?`,
-      initial: true,
-    });
-    if (!res.builtin) {
-      throw reason;
-    }
-
-    await createApp({
-      appPath: resolvedProjectPath,
-      packageManager,
-      typescript: program.typescript,
-    });
-  }
-};
-
-const update = checkForUpdate(packageJson).catch(() => null);
-
-const notifyUpdate = async (): Promise<void> => {
-  try {
-    const res = await update;
-    if (res?.latest) {
-      const pkgManager = getPkgManager();
-
-      console.log();
-      console.log(
-        chalk.yellow.bold('A new version of `create-next-app` is available!'),
-      );
-      console.log(
-        'You can update by running: ' +
-          chalk.cyan(
-            pkgManager === 'yarn'
-              ? 'yarn global add create-next-app'
-              : `${pkgManager} install --global create-next-app`,
-          ),
-      );
-      console.log();
-    }
-    process.exit();
-  } catch {
-    // ignore error
-  }
-};
-
-run()
-  .then(notifyUpdate)
-  .catch(async reason => {
-    console.log();
-    console.log('Aborting installation.');
-    if (reason.command) {
-      console.log(`  ${chalk.cyan(reason.command)} has failed.`);
-    } else {
-      console.log(chalk.red('Unexpected error. Please report it as a bug:'));
-      console.log(reason);
-    }
-    console.log();
-
-    await notifyUpdate();
-
+  if (packageManager !== 'yarn') {
+    console.error('Please use yarn to create swarmion app.');
     process.exit(1);
+  }
+
+  await createApp({
+    appPath: resolvedProjectPath,
+    packageManager,
   });
+};
+
+run().catch((reason: { command?: string }) => {
+  console.log();
+  console.log('Aborting installation.');
+  if (reason.command !== undefined) {
+    console.log(`  ${chalk.cyan(reason.command)} has failed.`);
+  } else {
+    console.log(chalk.red('Unexpected error. Please report it as a bug:'));
+    console.log(reason);
+  }
+  console.log();
+
+  process.exit(1);
+});
