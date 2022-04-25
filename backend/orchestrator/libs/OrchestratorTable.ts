656116dd08db340ea08ff57fac7c4a95ae68d858
fargito <fr.farge96@gmail.com>
2022-04-25 21:15:47 +0200

    fix(orchestrator): use generated name for table

--- backend/orchestrator/libs/OrchestratorTable.ts
+++ backend/orchestrator/libs/OrchestratorTable.ts
@@ -1,14 +1,14 @@
 import DynamoDB from 'aws-sdk/clients/dynamodb';
 import { Table } from 'dynamodb-toolbox';
 
-import { PARTITION_KEY, SORT_KEY } from 'resources/dynamodb';
+import { getEnvVariable } from '@swarmion/serverless-helpers';
 
-import { ORCHESTRATOR_TABLE_NAME } from './config/dynamodb';
+import { PARTITION_KEY, SORT_KEY } from 'resources/dynamodb';
 
 const DocumentClient = new DynamoDB.DocumentClient();
 
 export default new Table({
-  name: ORCHESTRATOR_TABLE_NAME,
+  name: getEnvVariable('ORCHESTRATOR_TABLE_NAME'),
   partitionKey: PARTITION_KEY,
   sortKey: SORT_KEY,
   autoExecute: true,
