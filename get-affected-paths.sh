824675a2d7640e04f70e7bb71640b56c3ad9b460
fargito <fr.farge96@gmail.com>
2022-05-06 18:34:28 +0200

    chore: add simple ci

---
+++ get-affected-paths.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -eo pipefail
+IFS=$'\n\t'
+
+readonly PROJECT_TYPE=$1
+
+# exit if PROJECT_TYPE not in 'apps' or 'libs'
+if [[ "$PROJECT_TYPE" != "apps" && "$PROJECT_TYPE" != "libs" ]]; then
+    echo "PROJECT_TYPE must be either 'apps' or 'libs'"
+    exit 1
+fi
+
+set -u
+
+replace() {
+    cat workspace.json | jq ".projects[\"$1\"]"
+}
+
+export -f replace
+
+readonly AFFECTED_STRING=$(yarn nx affected:"$PROJECT_TYPE" --plain)
+readonly AFFECTED_ARRAY=($(echo "$AFFECTED_STRING" | tr ' ' '\n'))
+
+RESULT=''
+
+for app in "${AFFECTED_ARRAY[@]}"; do
+    if [[ -z "${RESULT}" ]]; then
+        RESULT=$(replace "$app")
+    else
+        RESULT="$RESULT,$(replace "$app")"
+    fi
+done
+
+RESULT="[$RESULT]"
+
+echo "$RESULT"
