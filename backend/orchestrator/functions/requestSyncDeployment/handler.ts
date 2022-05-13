2d7ecabcab9270fa179cfd86805435bf5bae0b8c
axelf <axelf@theodo.fr>
2022-05-13 15:16:57 +0200

    refactor(orchestrator): use new contract api in orchestrator

--- backend/orchestrator/functions/requestSyncDeployment/handler.ts
+++ backend/orchestrator/functions/requestSyncDeployment/handler.ts
@@ -1,11 +1,12 @@
 import { ulid } from 'ulid';
 
 import { requestSyncDeployment } from '@swarmion/orchestrator-contracts';
+import { getInputSchema, HandlerType } from '@swarmion/serverless-contracts';
 import { applyHttpMiddlewares } from '@swarmion/serverless-helpers';
 
 import ServiceEventEntity from 'libs/dynamodb/models/serviceEvent';
 
-const handler = requestSyncDeployment.handler(async event => {
+const handler: HandlerType<typeof requestSyncDeployment> = async event => {
   const { serviceId, applicationId } = event.body;
 
   const eventId = ulid();
@@ -13,9 +14,9 @@ const handler = requestSyncDeployment.handler(async event => {
   await ServiceEventEntity.put({ serviceId, applicationId, eventId });
 
   return { status: 'ACCEPTED', message: 'processing' };
-});
+};
 
 export const main = applyHttpMiddlewares(handler, {
-  inputSchema: requestSyncDeployment.inputSchema,
+  inputSchema: getInputSchema(requestSyncDeployment),
   outputSchema: requestSyncDeployment.outputSchema,
 });
