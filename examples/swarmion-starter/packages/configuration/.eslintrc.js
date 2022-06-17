9adc518efe61685a0ac475f6112318a76e962a51
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-06-17 17:28:22 +0200

    feat(example): update swarmion-starter

--- examples/swarmion-starter/packages/configuration/.eslintrc.js
+++ examples/swarmion-starter/packages/configuration/.eslintrc.js
@@ -1,7 +1,4 @@
-const generateImportOrderRule = require('../../commonConfiguration/generateImportOrderRule');
-
 module.exports = {
-  rules: generateImportOrderRule(__dirname),
   parserOptions: {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
