5ee8d468eb24972f698ab9aa48b60806b2152ae2
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2022-07-08 16:54:07 +0200

    --wip-- [skip ci]

--- packages/serverless-contracts/src/contracts/events/eventContract.ts
+++ packages/serverless-contracts/src/contracts/events/eventContract.ts
@@ -1,11 +1,11 @@
 import { JSONSchema } from 'json-schema-to-ts';
 
-export class EventContract {
+export class EventContract<S extends JSONSchema = JSONSchema> {
   public contractType = 'event' as const;
   public id: string;
-  public payloadSchema: JSONSchema;
+  public payloadSchema: S;
 
-  constructor(props: { id: string; payloadSchema: JSONSchema }) {
+  constructor(props: { id: string; payloadSchema: S }) {
     this.id = props.id;
     this.payloadSchema = props.payloadSchema;
   }
--- packages/serverless-contracts/src/contracts/events/features/publishEventBridgeEvent.ts
+++ packages/serverless-contracts/src/contracts/events/features/publishEventBridgeEvent.ts
@@ -1,22 +1,29 @@
-import {Event, Bus} from 'typebridge';
 import { EventBridgeClient } from '@aws-sdk/client-eventbridge';
+import { FromSchema, JSONSchema } from 'json-schema-to-ts';
+import { Bus, Event } from 'typebridge';
 
 import { EventBridgeEventContract } from '../eventBridgeEventContract';
 
-export const publishEventBridgeEvent = <Contract extends EventBridgeEventContract>(
+export const publishEventBridgeEvent = async <
+  Contract extends EventBridgeEventContract,
+  S extends JSONSchema = Contract['payloadSchema'],
+>(
   contract: Contract,
   eventBridgeClient: EventBridgeClient,
   payload: Contract['payloadSchema'],
 ): Promise<void> => {
-  const {source, payloadSchema, name} = contract;
+  const { source, payloadSchema, name } = contract;
   const bus = new Bus({
     name: contract.busName,
-    EventBridge: new EventBridgeClient({}),
+    EventBridge: eventBridgeClient,
   });
-  const event = new Event<>({
+
+  const event = new Event<string, S, FromSchema<S>>({
     source,
     bus,
     schema: payloadSchema,
     name,
   });
+
+  await event.publish(payload);
 };
