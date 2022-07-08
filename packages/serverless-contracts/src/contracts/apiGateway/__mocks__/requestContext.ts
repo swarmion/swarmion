8f5d13b066d0c4f02a494aec2e056717979cd261
axelf <axelf@theodo.fr>
2022-07-08 18:08:06 +0200

    test(contracts): use common mocks in lambdaHandler tests

---
+++ packages/serverless-contracts/src/contracts/apiGateway/__mocks__/requestContext.ts
@@ -0,0 +1,61 @@
+import {
+  APIGatewayEventRequestContextV2WithAuthorizer,
+  APIGatewayEventRequestContextWithAuthorizer,
+} from 'aws-lambda';
+
+export const getRequestContextMock = (
+  args?: Partial<APIGatewayEventRequestContextWithAuthorizer<undefined>>,
+): APIGatewayEventRequestContextWithAuthorizer<undefined> => ({
+  accountId: '',
+  apiId: '',
+  authorizer: undefined,
+  protocol: '',
+  httpMethod: '',
+  identity: {
+    accessKey: null,
+    accountId: null,
+    apiKey: null,
+    apiKeyId: null,
+    caller: null,
+    clientCert: null,
+    cognitoAuthenticationProvider: null,
+    cognitoAuthenticationType: null,
+    cognitoIdentityId: null,
+    cognitoIdentityPoolId: null,
+    principalOrgId: null,
+    sourceIp: 'string',
+    user: null,
+    userAgent: null,
+    userArn: null,
+  },
+  path: '',
+  stage: '',
+  requestId: '',
+  requestTimeEpoch: 0,
+  resourceId: '',
+  resourcePath: '',
+  ...args,
+});
+
+export const getRequestContextMockV2 = (
+  args?: Partial<APIGatewayEventRequestContextV2WithAuthorizer<undefined>>,
+): APIGatewayEventRequestContextV2WithAuthorizer<undefined> => ({
+  authorizer: undefined,
+  accountId: '',
+  apiId: '',
+  domainName: '',
+  domainPrefix: '',
+  http: {
+    method: '',
+    path: '',
+    protocol: '',
+    sourceIp: '',
+    userAgent: '',
+  },
+  requestId: '',
+  routeKey: '',
+  stage: '',
+  time: '',
+  timeEpoch: 0,
+  ...args,
+});
