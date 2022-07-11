572ff309abf0e78f6bc4f154f399d917cdf9e820
fargito <fr.farge96@gmail.com>
2022-07-11 17:50:35 +0200

    chore(serverless-cdk-plugin): fix for jest 28

--- packages/serverless-cdk-plugin/jest.config.ts
+++ packages/serverless-cdk-plugin/jest.config.ts
@@ -1,3 +1,9 @@
 import { jestConfig } from '@swarmion/configuration';
 
-export default { ...jestConfig, testTimeout: 10000 };
+export default {
+  ...jestConfig,
+  testTimeout: 10000,
+  // This is a temporary fix for jest 28. See https://github.com/microsoft/accessibility-insights-web/pull/5421/commits/9ad4e618019298d82732d49d00aafb846fb6bac7
+  // We need to remove it as soon as possible. Check out https://github.com/facebook/jest/issues/9430
+  resolver: './integrationTests/jest28TemporaryResolver.js',
+};
