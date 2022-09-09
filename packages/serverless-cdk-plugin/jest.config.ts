dbd872126b2f788f829ace9c3d6e31f429641f8e
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-09-09 22:54:03 +0200

    chore(cdk-plugin): use standalone jest config
    We cannot migrate this package to vitest yet, until the following
    issue is resolved:
    https://github.com/vitest-dev/vitest/issues/1436

--- packages/serverless-cdk-plugin/jest.config.ts
+++ packages/serverless-cdk-plugin/jest.config.ts
@@ -1,3 +1,20 @@
-import { jestConfig } from '@swarmion/configuration';
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
 
-export default { ...jestConfig, testTimeout: 10000 };
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$';
+
+const config: InitialOptionsTsJest = {
+  preset: 'ts-jest',
+  testRegex: TEST_REGEX,
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  testEnvironment: 'node',
+  coverageReporters: ['json', ['lcov', { projectRoot: './' }]],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  testTimeout: 10000,
+};
+
+export default config;
