63bde9c164273b54ac3e7fb6d8af83cc241008b8
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-09-24 19:18:20 +0200

    test(serverless-cdk-plugin): adapt tests to pnpm

--- packages/serverless-cdk-plugin/integrationTests/wrongConstruct/customCdk.test.ts
+++ packages/serverless-cdk-plugin/integrationTests/wrongConstruct/customCdk.test.ts
@@ -1,9 +1,10 @@
 import runServerless from '@serverless/test/run-serverless';
+import { join } from 'path';
 
 describe('customCdK', () => {
   it('throws when serverlessConstruct is not a construct', async () => {
     await expect(
-      runServerless('../../node_modules/serverless', {
+      runServerless(join(__dirname, '../../node_modules/serverless'), {
         command: 'package',
         cwd: __dirname,
       }),
