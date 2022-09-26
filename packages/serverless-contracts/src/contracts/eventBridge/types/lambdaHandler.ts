593e2d730eca4562e9f18f9f04e3ee0fc800b8d8
fargito <fr.farge96@gmail.com>
2022-09-26 11:35:32 +0200

    feat(serverless-contracts): infer additionalArgs from inner function

--- packages/serverless-contracts/src/contracts/eventBridge/types/lambdaHandler.ts
+++ packages/serverless-contracts/src/contracts/eventBridge/types/lambdaHandler.ts
@@ -1,33 +1,27 @@
 import { Context, EventBridgeEvent, EventBridgeHandler } from 'aws-lambda';
 
-import { EventBridgeContract } from '../eventBridgeContract';
-import { PayloadType } from './common';
-
 export type HandlerType<
-  Contract extends EventBridgeContract,
-  AdditionalArgs extends never[] = never[],
+  EventType extends string,
+  Payload,
+  AdditionalArgs extends unknown[],
 > = (
-  event: HandlerEventType<Contract>,
+  event: EventBridgeEvent<EventType, Payload>,
   context: Context,
   ...additionalArgs: AdditionalArgs
 ) => Promise<unknown>;
 
-type HandlerEventType<Contract extends EventBridgeContract> = EventBridgeEvent<
-  Contract['eventType'],
-  PayloadType<Contract>
->;
-
-type EventBridgeHandlerParameters<Contract extends EventBridgeContract> =
-  Parameters<
-    EventBridgeHandler<Contract['eventType'], PayloadType<Contract>, unknown>
-  >;
+type EventBridgeHandlerParameters<
+  EventType extends string,
+  Payload,
+> = Parameters<EventBridgeHandler<EventType, Payload, unknown>>;
 
 export type EventBridgeHandlerType<
-  Contract extends EventBridgeContract,
-  AdditionalArgs extends never[] = never[],
+  EventType extends string,
+  Payload,
+  AdditionalArgs extends unknown[],
 > = (
-  event: EventBridgeHandlerParameters<Contract>[0],
-  context: EventBridgeHandlerParameters<Contract>[1],
-  callback: EventBridgeHandlerParameters<Contract>[2],
+  event: EventBridgeHandlerParameters<EventType, Payload>[0],
+  context: EventBridgeHandlerParameters<EventType, Payload>[1],
+  callback: EventBridgeHandlerParameters<EventType, Payload>[2],
   ...additionalArgs: AdditionalArgs
 ) => Promise<unknown>;
