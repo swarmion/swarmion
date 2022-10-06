09522a45e9bd14292a57650dc9cd19b27beb343b
axelf <axelf@theodo.fr>
2022-10-06 02:06:54 +0200

    feat(full-stack): use new getHandler feature

--- examples/swarmion-full-stack/backend/users/functions/getUser/handler.ts
+++ examples/swarmion-full-stack/backend/users/functions/getUser/handler.ts
@@ -1,17 +1,11 @@
-import { getLambdaHandler } from '@swarmion/serverless-contracts';
-import { applyHttpMiddlewares } from '@swarmion/serverless-helpers';
+import { getHandler } from '@swarmion/serverless-contracts';
 
 import { getUserContract } from '@swarmion-full-stack/users-contracts';
 
-const handler = getLambdaHandler(getUserContract)(async event => {
+export const main = getHandler(getUserContract)(async event => {
   const { userId } = event.pathParameters;
 
   await Promise.resolve({ userId });
 
   return { userId, userName: 'hello_world' };
-});
-
-export const main = applyHttpMiddlewares(handler, {
-  inputSchema: getUserContract.inputSchema,
-  outputSchema: getUserContract.outputSchema,
 });
