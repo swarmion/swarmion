7f66aec5589839d58e284d1210c813d21977cea6
yonadav <35917238+yehonadav@users.noreply.github.com>
2022-10-16 15:20:21 +0300

    Update getHandlerPath.ts
    make it cross-platform

--- packages/serverless-helpers/src/helpers/getHandlerPath.ts
+++ packages/serverless-helpers/src/helpers/getHandlerPath.ts
@@ -1,8 +1,10 @@
+import * as path from "path";
+
 /** Helper to be used in config.ts files to retrieve the path of the handler. */
 const getHandlerPath = (directoryPath: string): string => {
   const processRunLocation = process.cwd();
 
-  return directoryPath.replace(processRunLocation + '/', '') + '/handler.main';
+  return directoryPath.replace(processRunLocation + path.sep, '') + path.sep + 'handler.main';
 };
 
 export default getHandlerPath;
