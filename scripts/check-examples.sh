0ea5fea2888c049376c06f307063ace4765bc83a
axelf <axelf@theodo.fr>
2022-10-18 15:20:42 +0200

    feat: add ci job to check create-app examples

--- scripts/check-examples.sh
+++ scripts/check-examples.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for folder in examples/* ; do
-  cd $folder
-  pnpm install
-  pnpm package
+set -e
+
+pnpm nx run create-swarmion-app:build
+mkdir -p ./dist
+
+for template in swarmion-starter swarmion-full-stack; do
+  NO_PNPM_POSTINSTALL=true pnpm node packages/create-swarmion-app/dist/index.js -t $template -s $REF ./dist/$template-app
+  cd ./dist/$template-app
   pnpm test
   cd ../..
-done;
+done
