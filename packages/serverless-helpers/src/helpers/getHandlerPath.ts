c341ad1c44147e16f43c073439d4c62fdb086150
yonadav <35917238+yehonadav@users.noreply.github.com>
2022-10-20 07:57:34 +0300

    Update getHandlerPath.ts
    fix(deployment) prevent Runtime.ImportModuleError when windows machine path is deployed to a linux executor

--- packages/serverless-helpers/src/helpers/getHandlerPath.ts
+++ packages/serverless-helpers/src/helpers/getHandlerPath.ts
@@ -1,10 +1,8 @@
-import * as path from "path";
-
 /** Helper to be used in config.ts files to retrieve the path of the handler. */
 const getHandlerPath = (directoryPath: string): string => {
   const processRunLocation = process.cwd();
 
-  return directoryPath.replace(processRunLocation + path.sep, '') + path.sep + 'handler.main';
+  return directoryPath.replace(processRunLocation + '/', '') + '/handler.main';
 };
 
 export default getHandlerPath;
