5caedeaab916426464683963c6213fda6d45eb08
fargito <fr.farge96@gmail.com>
2022-10-24 22:14:23 +0200

    feat(serverless-contracts): add exhaustiveness checks

--- packages/serverless-contracts/src/features/lambdaTrigger.ts
+++ packages/serverless-contracts/src/features/lambdaTrigger.ts
@@ -14,7 +14,14 @@ export const getTrigger = <Contract extends ServerlessContract>(
       // @ts-ignore inference is not good enough here, overriding
       return getEventBridgeTrigger(contract, additionalConfig);
 
+    case 'cloudFormation':
+      throw new Error('EventBridge contract has no trigger');
+
     default:
+      // exhaustiveness check
+      // eslint-disable-next-line no-case-declarations
+      const _neverContract: never = contract;
+      console.error('Not implemented for contract', _neverContract);
       throw new Error('Not implemented');
   }
 };
