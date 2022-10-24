073bc3a09c3d4f4c4821e8f1adc4b9b733ef79fa
fargito <fr.farge96@gmail.com>
2022-10-24 21:07:54 +0200

    feat(serverless-contracts): add function overloading for getHandler

---
+++ packages/serverless-contracts/src/features/lambdaHandler.ts
@@ -0,0 +1,54 @@
+import {
+  ApiGatewayContract,
+  EventBridgeContract,
+  getApiGatewayHandler,
+  getEventBridgeHandler,
+} from 'contracts';
+import {
+  ApiGatewayHandler,
+  HandlerType as ApiGatewayInnerHandlerType,
+} from 'contracts/apiGateway/types/lambdaHandler';
+import { PayloadType } from 'contracts/eventBridge/types/common';
+import {
+  EventBridgeHandlerType,
+  HandlerType as EventBridgeInnerHandlerType,
+} from 'contracts/eventBridge/types/lambdaHandler';
+import { ServerlessContract } from 'types';
+
+/**
+ * must match the type of getApiGatewayHandler
+ */
+function getHandler<Contract extends ApiGatewayContract>(
+  contract: Contract,
+): (
+  handler: ApiGatewayInnerHandlerType<Contract>,
+) => ApiGatewayHandler<Contract>;
+
+/**
+ * must match the type of getEventBridgeHandler
+ */
+function getHandler<
+  Contract extends EventBridgeContract,
+  EventType extends string = Contract['eventType'],
+  Payload = PayloadType<Contract>,
+>(
+  contract: Contract,
+): <AdditionalArgs extends unknown[]>(
+  handler: EventBridgeInnerHandlerType<EventType, Payload, AdditionalArgs>,
+) => EventBridgeHandlerType<EventType, Payload, AdditionalArgs>;
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+function getHandler<Contract extends ServerlessContract>(
+  contract: Contract,
+): unknown {
+  switch (contract.contractType) {
+    case 'eventBridge':
+      return getEventBridgeHandler(contract);
+    case 'apiGateway':
+      return getApiGatewayHandler(contract);
+    default:
+      throw new Error('Not implemented');
+  }
+}
+
+export { getHandler };
