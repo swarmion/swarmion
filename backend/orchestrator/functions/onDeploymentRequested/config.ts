b63b2c88ac46ca34a115d3967c9802bbb5ddc149
fargito <fr.farge96@gmail.com>
2022-10-26 20:33:43 +0200

    feat(orchestrator): add event bus name to trigger

--- backend/orchestrator/functions/onDeploymentRequested/config.ts
+++ backend/orchestrator/functions/onDeploymentRequested/config.ts
@@ -2,11 +2,17 @@ import { onDeploymentRequestedContract } from '@swarmion/orchestrator-contracts'
 import { getTrigger } from '@swarmion/serverless-contracts';
 import { getHandlerPath, LambdaFunction } from '@swarmion/serverless-helpers';
 
+import { getCdkProperty } from 'resources';
+
 const config: LambdaFunction = {
   environment: {},
   handler: getHandlerPath(__dirname),
   iamRoleStatementsInherit: true,
-  events: [getTrigger(onDeploymentRequestedContract, { eventBus: 'toto' })],
+  events: [
+    getTrigger(onDeploymentRequestedContract, {
+      eventBus: getCdkProperty('eventBusName'),
+    }),
+  ],
 };
 
 export default config;
