a528a210821d0c1308bf65a2703cbad2b1e32c25
Guillaume Duboc <guillaumed@theodo.fr>
2022-11-08 14:31:57 +0100

    chore(swarmion-with-next): use absolute imports

--- examples/swarmion-with-next/frontend/next-app/src/pages/_app.tsx
+++ examples/swarmion-with-next/frontend/next-app/src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+
+import 'styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
