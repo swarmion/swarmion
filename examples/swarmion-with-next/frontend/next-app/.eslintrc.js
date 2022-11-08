864b44b3867661dcb732358331798bf78fe7077c
Guillaume Duboc <guillaumed@theodo.fr>
2022-11-08 10:18:42 +0100

    chore(swarmion-with-next): add linter rules

--- examples/swarmion-with-next/frontend/next-app/.eslintrc.js
+++ examples/swarmion-with-next/frontend/next-app/.eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
+  extends: [
+    'next/core-web-vitals',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  plugins: ['react-hooks', 'jsx-a11y', 'risxss'],
+  rules: {
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'error',
+    'jsx-a11y/anchor-is-valid': 'error',
+    'jsx-a11y/accessible-emoji': 'off',
+    'risxss/catch-potential-xss-react': 'error',
+    'react/jsx-curly-brace-presence': 'error',
+  },
+  settings: { react: { version: 'detect' } },
+
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
 };
