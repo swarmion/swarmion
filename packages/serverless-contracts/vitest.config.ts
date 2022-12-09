197c1072ff22d347dff15975e89252e068b1ad8e
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2022-12-09 18:06:22 +0100

    test: add coverage

--- packages/serverless-contracts/vitest.config.ts
+++ packages/serverless-contracts/vitest.config.ts
@@ -4,6 +4,7 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    coverage: { reporter: ['text-summary', 'lcovonly'] },
     globals: true,
     exclude: [...configDefaults.exclude, '**/*.type.test.ts'],
   },
