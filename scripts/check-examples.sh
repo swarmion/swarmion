4be758690f96de3b2ca17bde140f92dac1b038f8
fargito <fr.farge96@gmail.com>
2022-12-12 11:37:20 +0100

    test: include build command in check-examples script

--- scripts/check-examples.sh
+++ scripts/check-examples.sh
@@ -9,5 +9,6 @@ for template in swarmion-starter swarmion-full-stack; do
   NO_PNPM_POSTINSTALL=true pnpm node packages/create-swarmion-app/dist/index.js -t $template -s $REF ./dist/$template-app
   cd ./dist/$template-app
   pnpm test
+  pnpm build
   cd ../..
 done
