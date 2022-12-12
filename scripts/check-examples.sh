4d7ee8751b3c19f40d873854cbfe49bfe0ca9222
fargito <fr.farge96@gmail.com>
2022-12-12 16:01:35 +0100

    ci: use tmp subdirectory for examples check

--- scripts/check-examples.sh
+++ scripts/check-examples.sh
@@ -3,12 +3,15 @@
 set -e
 
 pnpm nx run create-swarmion-app:build
-mkdir -p ./dist
+BASE_DIR=$(pwd)
+TEMP_DIR=$(mktemp -d)
 
 for template in swarmion-starter swarmion-full-stack; do
-  NO_PNPM_POSTINSTALL=true pnpm node packages/create-swarmion-app/dist/index.js -t $template -s $REF ./dist/$template-app
-  cd ./dist/$template-app
+  NO_PNPM_POSTINSTALL=true pnpm node packages/create-swarmion-app/dist/index.js -t $template -s $REF $TEMP_DIR/$template-app
+  cd $TEMP_DIR/$template-app
   pnpm test
   pnpm build
-  cd ../..
+  cd $BASE_DIR
 done
+
+rm -rf $TEMP_DIR
