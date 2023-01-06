1da2359bc5e8ea7faed1d03bcb5e93552d687701
axelf <axelf@theodo.fr>
2023-01-06 18:23:07 +0100

    chore(ci): trigger check examples with script update

--- scripts/check-examples.sh
+++ scripts/check-examples.sh
@@ -6,6 +6,8 @@ pnpm nx run create-swarmion-app:build
 BASE_DIR=$(pwd)
 TEMP_DIR=$(mktemp -d)
 
+# We loop through the examplesn, build them and run their tests
+
 for template in swarmion-starter swarmion-full-stack; do
   NO_PNPM_POSTINSTALL=true pnpm node packages/create-swarmion-app/dist/index.js -t $template -s $REF $TEMP_DIR/$template-app
   cd $TEMP_DIR/$template-app
