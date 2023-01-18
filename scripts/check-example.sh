071087632fbf24a40fc3cce74cda9f5bc46679a1
fargito <fr.farge96@gmail.com>
2023-01-18 17:46:12 +0100

    chore(check-examples): add random id

--- scripts/check-example.sh
+++ scripts/check-example.sh
@@ -5,8 +5,10 @@ set -e
 pnpm nx run create-swarmion-app:build
 BASE_DIR=$(pwd)
 TEMP_DIR=$(mktemp -d)
+RANDOM_ID=$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 6)
+EXAMPLE_NAME=$EXAMPLE-$RANDOM_ID
 
-NO_PNPM_POSTINSTALL=true pnpm node packages/create-swarmion-app/dist/index.js -t $EXAMPLE -s $REF $TEMP_DIR/$EXAMPLE-app
-cd $TEMP_DIR/$EXAMPLE-app
+NO_PNPM_POSTINSTALL=true pnpm node packages/create-swarmion-app/dist/index.js -t $EXAMPLE -s $REF $TEMP_DIR/$EXAMPLE_NAME
+cd $TEMP_DIR/$EXAMPLE_NAME
 pnpm test
 pnpm build
