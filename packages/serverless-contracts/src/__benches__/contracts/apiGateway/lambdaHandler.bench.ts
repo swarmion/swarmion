b46143b26dbde931dff7c2436bf1075945c26dab
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-01-22 17:24:14 +0100

    chore(serverless-contracts): add a first real bench

---
+++ packages/serverless-contracts/src/__benches__/contracts/apiGateway/lambdaHandler.bench.ts
@@ -0,0 +1,64 @@
+import {
+  APIGatewayEventRequestContextV2WithAuthorizer,
+  APIGatewayProxyCognitoAuthorizer,
+} from 'aws-lambda';
+
+import { getAPIGatewayV2EventRequestContextMock } from '@swarmion/serverless-helpers';
+
+import { getHandlerContextMock } from '__mocks__/requestContext';
+import { httpApiGatewayContractMock } from 'contracts/apiGateway/__mocks__/httpApiGatewayContract';
+import { getHandler } from 'features';
+
+const httpApiContract = httpApiGatewayContractMock;
+
+const fakeRequestContext: APIGatewayEventRequestContextV2WithAuthorizer<APIGatewayProxyCognitoAuthorizer> =
+  {
+    ...getAPIGatewayV2EventRequestContextMock(),
+    accountId: '123456789012' as const,
+    authorizer: { claims: { foo: 'claimBar' } },
+  };
+const fakeContext = getHandlerContextMock();
+
+const httpHandler = getHandler(httpApiContract)(
+  async ({
+    body,
+    pathParameters,
+    queryStringParameters,
+    headers,
+    requestContext,
+  }) => {
+    await Promise.resolve();
+    const myCustomClaim = requestContext.authorizer.claims.foo;
+
+    const name =
+      body.foo +
+      pathParameters.pageNumber +
+      queryStringParameters.testId +
+      headers.myHeader +
+      myCustomClaim;
+
+    return Promise.resolve({ id: 'hello', name });
+  },
+);
+
+export const basicHttpApiHandler = async (): Promise<void> => {
+  await httpHandler(
+    {
+      pathParameters: { userId: 'toto', pageNumber: '15' },
+      body: JSON.stringify({ foo: 'bar' }),
+      headers: {
+        myHeader: 'MyCustomHeader',
+        anotherHeader: 'anotherHeader',
+      },
+      queryStringParameters: { testId: 'myTestId' },
+      requestContext: fakeRequestContext,
+      version: '',
+      routeKey: '',
+      rawPath: '',
+      rawQueryString: '',
+      isBase64Encoded: false,
+    },
+    fakeContext,
+    () => null,
+  );
+};
--- packages/serverless-contracts/src/__benches__/index.ts
+++ packages/serverless-contracts/src/__benches__/index.ts
@@ -1,23 +1,14 @@
 import { withCodSpeed } from '@codspeed/tinybench-plugin';
 import { Bench } from 'tinybench';
 
-const fibonacci = (n: number): number => {
-  if (n < 2) {
-    return n;
-  }
-
-  return fibonacci(n - 1) + fibonacci(n - 2);
-};
+import { basicHttpApiHandler } from './contracts';
 
 const bench = withCodSpeed(new Bench());
 
-bench
-  .add('fibonacci10', () => {
-    fibonacci(10);
-  })
-  .add('fibonacci15', () => {
-    fibonacci(15);
-  });
+bench.add(
+  'contracts > ApiGatewayContract > basic handler',
+  basicHttpApiHandler,
+);
 
 await bench.run();
 console.table(
