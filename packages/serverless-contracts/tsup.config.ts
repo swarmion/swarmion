fa8d849ec545a7cfb4e3c72b98ac86b69ce698af
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-01-22 17:18:30 +0100

    chore(serverless-contracts): switch package to esm

--- packages/serverless-contracts/tsup.config.ts
+++ packages/serverless-contracts/tsup.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   clean: true,
   format: ['cjs', 'esm'],
-  outExtension: ctx => {
-    return { js: `.${ctx.format}.js` };
-  },
   outDir: 'dist',
 });
