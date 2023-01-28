8865443ec48a4c95f17f19590658ddc3c4d6585a
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-01-28 17:25:03 +0100

    feat(create-swarmion-app): add swarmion-bare (#496)

--- packages/create-swarmion-app/src/getProjectTemplate.ts
+++ packages/create-swarmion-app/src/getProjectTemplate.ts
@@ -1,13 +1,39 @@
 // eslint-disable-next-line import/no-named-as-default
-import prompts from 'prompts';
+import prompts, { Choice } from 'prompts';
 
 const TEMPLATES = [
   'swarmion-starter',
   'swarmion-full-stack',
   'swarmion-with-next',
+  'swarmion-bare',
 ] as const;
 
 export type Template = typeof TEMPLATES[number];
+
+const choices: (Omit<Choice, 'value'> & { value: Template })[] = [
+  {
+    title: 'Swarmion Starter',
+    description: 'Simple example with a single Serverless Framework backend',
+    value: 'swarmion-starter',
+  },
+  {
+    title: 'Swarmion Fullstack',
+    description:
+      'More complete example with a backend, a frontend and a shared lib',
+    value: 'swarmion-full-stack',
+  },
+  {
+    title: 'Swarmion with Next.js',
+    description: 'Example with a backend and a frontend using Next.js',
+    value: 'swarmion-with-next',
+  },
+  {
+    title: 'Swarmion Bare',
+    description:
+      'Minimalist example without any service, start with this for maximum flexibility',
+    value: 'swarmion-bare',
+  },
+];
 
 const isValidTemplate = (template: unknown): template is Template =>
   typeof template === 'string' && TEMPLATES.includes(template as Template);
@@ -23,24 +49,7 @@ const getProjectTemplate = async (
     type: 'select',
     name: 'template',
     message: 'Choose your starting template',
-    choices: [
-      {
-        title: 'Swarmion Starter',
-        description: 'Simple example with a single backend',
-        value: 'swarmion-starter',
-      },
-      {
-        title: 'Swarmion Fullstack',
-        description:
-          'More complete example with a backend, a frontend and a shared lib',
-        value: 'swarmion-full-stack',
-      },
-      {
-        title: 'Swarmion with Next.js',
-        description: 'Example with a backend and a frontend using Next.js',
-        value: 'swarmion-with-next',
-      },
-    ],
+    choices,
   });
 
   if (isValidTemplate(templateRes.template)) {
