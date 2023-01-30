678bbc46e1cd09579e29f00bebfbba375ceb0ee5
François Farge <fr.farge96@gmail.com>
2023-01-30 10:46:55 +0100

    chore(tsup): silent output (#500)

--- examples/swarmion-bare/packages/serverless-configuration/tsup.config.ts
+++ examples/swarmion-bare/packages/serverless-configuration/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
+  silent: true,
   format: ['cjs', 'esm'],
   outExtension: ctx => {
     return { js: `.${ctx.format}.js` };
--- packages/serverless-contracts/tsup.config.ts
+++ packages/serverless-contracts/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
+  silent: true,
   format: ['cjs', 'esm'],
   outDir: 'dist',
 });
