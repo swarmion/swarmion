abf77120341e79dacc17cba024aa9317e2549c46
fargito <fr.farge96@gmail.com>
2023-02-10 22:36:33 +0100

    fix(serverless-contracts): remove mock handler export

--- packages/serverless-contracts/src/contracts/apiGateway/index.ts
+++ packages/serverless-contracts/src/contracts/apiGateway/index.ts
@@ -7,7 +7,8 @@ export {
   getLambdaHandler,
   getApiGatewayHandler,
   getRequestParameters,
-  getMockHandlerInput,
-  setMockHandlerInputSeed,
+  // TODO add this back. For context, see https://github.com/swarmion/swarmion/issues/527
+  // getMockHandlerInput,
+  // setMockHandlerInputSeed,
 } from './features';
 export type { SwarmionApiGatewayHandler, ApiGatewayHandler } from './types';
