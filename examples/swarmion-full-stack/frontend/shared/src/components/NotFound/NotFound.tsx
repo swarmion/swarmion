8116806ba5ba493c81c3988c742f12542db9a2ca
Guillaume Duboc <guillaumed@theodo.fr>
2023-02-10 16:43:10 +0100

    examples(swarmion-full-stack): add png to shared library

--- examples/swarmion-full-stack/frontend/shared/src/components/NotFound/NotFound.tsx
+++ examples/swarmion-full-stack/frontend/shared/src/components/NotFound/NotFound.tsx
@@ -1,11 +1,12 @@
 import { Typography } from '@mui/material';
 
-import { NotFoundIcon } from 'assets';
+import { NotFoundIcon, notFoundPngUrl } from 'assets';
 
 const NotFound = (): JSX.Element => (
   <>
     <NotFoundIcon />
     <Typography variant="h1">Page not found</Typography>
+    <img src={notFoundPngUrl} alt="page not found png" width="180px" />
   </>
 );
 
--- examples/swarmion-full-stack/frontend/shared/src/components/NotFound/__tests__/NotFound.test.tsx
+++ examples/swarmion-full-stack/frontend/shared/src/components/NotFound/__tests__/NotFound.test.tsx
@@ -4,6 +4,7 @@ import NotFound from '../NotFound';
 
 vi.mock('assets', () => ({
   NotFoundIcon: 'svg',
+  notFoundPngUrl: 'png',
 }));
 
 describe('NotFound component', () => {
