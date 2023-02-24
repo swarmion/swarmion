aa3a2336d80fbc07929ddbc3b17d7661b51ffdae
axelf <axelf@theodo.fr>
2023-02-24 16:03:22 +0100

    refactor(serverless-contracts-plugin): use aggregate feature directly

--- packages/serverless-contracts-plugin/src/plugin/utils/generateOpenApiDocumentation.ts
+++ packages/serverless-contracts-plugin/src/plugin/utils/generateOpenApiDocumentation.ts
@@ -1,11 +1,8 @@
-import { OpenAPIV3 } from 'openapi-types';
 import Serverless from 'serverless';
 
 import { getOpenApiDocumentation } from '@swarmion/serverless-contracts';
 
 import { ServerlessContracts } from 'types/serviceOptions';
-
-import { isDefined } from './isDefined';
 
 export const generateOpenApiDocumentation = (serverless: Serverless): void => {
   // @ts-ignore mistype in the orignals (the animals)
@@ -13,31 +10,12 @@ export const generateOpenApiDocumentation = (serverless: Serverless): void => {
   const { provides } = serverless.service.initialServerlessConfig
     .contracts as ServerlessContracts;
 
-  const contractDocumentations = Object.values(provides)
-    .map(contract => getOpenApiDocumentation(contract))
-    .filter(isDefined);
+  const openApiDocumentation = getOpenApiDocumentation({
+    title: serverless.service.getServiceName(),
+    description: (serverless.service.initialServerlessConfig as Serverless)
+      .resources.Description,
+    contracts: Object.values(provides),
+  });
 
-  const paths: OpenAPIV3.PathsObject =
-    contractDocumentations.reduce<OpenAPIV3.PathsObject>(
-      (pathsObject, pathDocumentation) => {
-        pathsObject[pathDocumentation.path] = {
-          [pathDocumentation.method]: pathDocumentation.documentation,
-        };
-
-        return pathsObject;
-      },
-      {},
-    );
-
-  const openApiDocumentation: OpenAPIV3.Document = {
-    openapi: '3.0.1',
-    info: {
-      title: serverless.service.getServiceName(),
-      description: (serverless.service.initialServerlessConfig as Serverless)
-        .resources.Description,
-      version: new Date().toISOString(),
-    },
-    paths,
-  };
   console.log(JSON.stringify(openApiDocumentation, null, 2));
 };
