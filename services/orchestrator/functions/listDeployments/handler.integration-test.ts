d33075c14a347671f4a4273bb2628b45bd17e859
CorentinDoue <corentin.doue.pro@gmail.com>
2023-03-31 16:48:47 +0200

    test(orchestrator): add integration tests

---
+++ services/orchestrator/functions/listDeployments/handler.integration-test.ts
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch';
+import { expect } from 'vitest';
+
+import { TEST_ENV_VARS } from 'testEnvVars';
+
+describe('listDeployments', () => {
+  it('should put a service event entity inside the orchestrator table', async () => {
+    const response = await fetch(`${TEST_ENV_VARS.API_URL}/deployments`);
+    expect(response.status).toBe(200);
+    const body = await response.json();
+    expect(body).toEqual({ id: 'coucou' });
+  });
+});
