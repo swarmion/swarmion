917240fc2be518e049d573816beba7c497323cc3
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-04-25 16:24:41 +0200

    chore(orchestrator): add listDeployments unit tests

---
+++ services/orchestrator/functions/listDeployments/handler.test.ts
@@ -0,0 +1,47 @@
+import { listDeploymentsContract } from '@swarmion/orchestrator-contracts';
+import { getMockHandlerInput } from '@swarmion/serverless-contracts/test-utils';
+
+import { main } from './handler';
+
+beforeEach(() => {
+  console.error = vi.fn();
+  console.log = vi.fn();
+});
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+describe('listDeployments handler', () => {
+  it('should return a list of deployments', async () => {
+    const result = await main(
+      ...getMockHandlerInput(listDeploymentsContract, {
+        queryStringParameters: { applicationId: 'applicationId' },
+      }),
+    );
+
+    expect(console.log).toHaveBeenCalledWith({
+      applicationId: 'applicationId',
+    });
+
+    expect(result).toStrictEqual({
+      body: JSON.stringify({ id: 'coucou' }),
+      headers: { 'Content-Type': 'application/json' },
+      statusCode: 200,
+    });
+  });
+
+  it('should return an error when a non-existing param is passed', async () => {
+    const result = await main(
+      ...getMockHandlerInput(listDeploymentsContract, {
+        queryStringParameters: { nonExistingParam: 'foo' },
+      }),
+    );
+
+    expect(result).toStrictEqual({
+      body: 'Invalid input',
+      headers: undefined,
+      statusCode: 400,
+    });
+  });
+});
