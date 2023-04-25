748994cf14c4933c6a96906dc1e7ae325c91940c
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-04-25 16:23:41 +0200

    chore(orchestrator): use profile from env in integration tests first

--- services/orchestrator/setupIntegration.ts
+++ services/orchestrator/setupIntegration.ts
@@ -4,7 +4,7 @@ import { syncTestEnvVars } from '@swarmion/integration-tests';
 import { sharedParams } from '@swarmion/serverless-configuration';
 
 if (process.env.CI !== 'true') {
-  process.env.AWS_PROFILE = sharedParams.dev.profile;
+  process.env.AWS_PROFILE ??= sharedParams.dev.profile;
 }
 
 await syncTestEnvVars({
