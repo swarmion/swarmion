319e999e9df6dc19d7e416c902a31a4f73605bcc
valentinbeggi <valentinbe@theodo.fr>
2023-04-28 15:47:32 +0200

    chore(swarmion-cdk-starter): add code service

---
+++ examples/swarmion-starter-cdk/services/core/functions/config.ts
@@ -0,0 +1,1 @@
+export { Health } from './health/config';
---
+++ examples/swarmion-starter-cdk/services/core/functions/health/config.ts
@@ -0,0 +1,35 @@
+import { getCdkHandlerPath } from '@swarmion/serverless-helpers';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+
+import { sharedCdkEsbuildConfig } from '@swarmion-starter-cdk/serverless-configuration';
+
+import { healthContract } from 'contracts/healthContract';
+
+type HealthProps = { restApi: RestApi };
+
+export class Health extends Construct {
+  public healthFunction: NodejsFunction;
+
+  constructor(scope: Construct, id: string, { restApi }: HealthProps) {
+    super(scope, id);
+
+    this.healthFunction = new NodejsFunction(this, 'Lambda', {
+      entry: getCdkHandlerPath(__dirname),
+      handler: 'main',
+      runtime: Runtime.NODEJS_16_X,
+      architecture: Architecture.ARM_64,
+      awsSdkConnectionReuse: true,
+      bundling: sharedCdkEsbuildConfig,
+    });
+
+    restApi.root
+      .resourceForPath(healthContract.path)
+      .addMethod(
+        healthContract.method,
+        new LambdaIntegration(this.healthFunction),
+      );
+  }
+}
