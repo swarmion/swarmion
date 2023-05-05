45dc388dec1f693aceedc4e73664ef9b4c6d1f06
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-05-05 17:49:58 +0200

    feat(nx-plugin): bump vite-tsconfig-paths

--- packages/nx-plugin/src/generators/helpers/versions.ts
+++ packages/nx-plugin/src/generators/helpers/versions.ts
@@ -3,5 +3,5 @@ export const typescriptVersion = '^5.0.0';
 
 // vitest
 export const vitestVersion = '0.30.0';
-export const viteTsConfigPathsVersion = '^4.0.3';
+export const viteTsConfigPathsVersion = '^4.2.0';
 export const vitestCoverageC8Version = '0.30.0';
