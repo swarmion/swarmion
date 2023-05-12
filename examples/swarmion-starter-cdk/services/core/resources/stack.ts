71eccdaaa53e8d954f52fe5af95be4702c78234b
valentinbeggi <valentinbe@theodo.fr>
2023-05-12 16:49:08 +0200

    chore: use getAppStage

--- examples/swarmion-starter-cdk/services/core/resources/stack.ts
+++ examples/swarmion-starter-cdk/services/core/resources/stack.ts
@@ -9,21 +9,23 @@ import {
 
 import { Health } from 'functions/config';
 
-interface CoreProps {
-  stage: string;
-}
-
 export class CoreStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps & CoreProps) {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const { stage } = props;
+    const stage = getAppStage(this);
+    const {
+      restApiConfig: { allowedOrigins },
+    } = getAppConfig(this);
 
     const coreApi = new RestApi(this, 'CoreApi', {
       // the stage of the API is the same as the stage of the stack
       description: `Core API - ${stage}`,
       deployOptions: {
         stageName: stage,
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: allowedOrigins,
       },
     });
 
