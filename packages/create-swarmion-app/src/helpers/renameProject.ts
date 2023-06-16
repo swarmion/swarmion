6d3ce29a147046487b43d72a5b49dcce24372de1
Guillaume Lagrange <guillaumelag@theodo.fr>
2023-06-16 22:13:58 +0200

    fix(create-swarmion-app): ignore lock-file in renameProject
    We use the `@` prefix to match the project name in the lockfile to avoid
    accidental matches in integrity hashes.
    Also install the examples with a frozen lockfile to detect errors
    locally.

--- packages/create-swarmion-app/src/helpers/renameProject.ts
+++ packages/create-swarmion-app/src/helpers/renameProject.ts
@@ -10,7 +10,12 @@ export const renameProject = (
     cwd: root,
   });
   execSync(
-    `grep -rl '${template}'|xargs perl -i -pe's/${template}/${appName}/g'`,
+    `grep -rl '${template}' --exclude "pnpm-lock.yaml" | xargs perl -i -pe's/${template}/${appName}/g'`,
     { stdio: 'ignore', cwd: root },
   );
+  // Use @ prefix in pnpm-lock to avoid accidental matches in integrity hashes
+  execSync(`perl -i -pe's/\\@${template}/\\@${appName}/g' pnpm-lock.yaml`, {
+    stdio: 'ignore',
+    cwd: root,
+  });
 };
