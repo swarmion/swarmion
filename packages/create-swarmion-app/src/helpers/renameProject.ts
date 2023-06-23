baba1eb9ddebdfa1db019ca92f71c08bba7aa60b
Guillaume Lagrange <guillaumelag@theodo.fr>
2023-06-23 18:12:22 +0200

    fix(create-swarmion-app): allow creation of app with same name as template

--- packages/create-swarmion-app/src/helpers/renameProject.ts
+++ packages/create-swarmion-app/src/helpers/renameProject.ts
@@ -5,10 +5,12 @@ export const renameProject = (
   template: string,
   root: string,
 ): void => {
-  execSync(`mv ${template}.code-workspace ${appName}.code-workspace`, {
-    stdio: 'ignore',
-    cwd: root,
-  });
+  if (template !== appName) {
+    execSync(`mv ${template}.code-workspace ${appName}.code-workspace`, {
+      stdio: 'ignore',
+      cwd: root,
+    });
+  }
   execSync(
     `grep -rl '${template}' --exclude "pnpm-lock.yaml" | xargs perl -i -pe's/${template}/${appName}/g'`,
     { stdio: 'ignore', cwd: root },
