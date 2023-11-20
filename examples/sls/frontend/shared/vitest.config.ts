3c1f9e42b8c9faf7ceef38afa55f40c5a831d552
fargito <fr.farge96@gmail.com>
2023-11-20 12:43:37 +0100

    examples(sls): add tsconfig plugin to shared library

--- examples/sls/frontend/shared/vitest.config.ts
+++ examples/sls/frontend/shared/vitest.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), svgrPlugin()],
   test: {
     globals: true,
     environment: 'jsdom',
--- examples/sls/frontend/shared/vitest.setup.ts
+++ examples/sls/frontend/shared/vitest.setup.ts
@@ -1,4 +1,8 @@
 import '@testing-library/jest-dom/extend-expect';
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+
+expect.extend(matchers);
 
 vi.mock('assets', () => ({
   notFoundJpgUrl: 'notFoundJpgUrl',
