702a53f79b1053326c4c97019cc23bf3fe736cf4
fargito <fr.farge96@gmail.com>
2023-11-27 11:01:25 +0100

    feat(documentation): add custom domain names

---
+++ user-docs/documentation/hosting/utils/getDomainNames.ts
@@ -0,0 +1,12 @@
+import { AppStage } from './getAppStage';
+
+export const getDomainNames = (stage: AppStage): string[] => {
+  switch (stage) {
+    case 'dev':
+      return [];
+    case 'staging':
+      return ['staging.swarmion.dev'];
+    case 'production':
+      return ['www.swarmion.dev'];
+  }
+};
