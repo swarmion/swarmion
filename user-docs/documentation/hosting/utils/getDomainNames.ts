35d41ea2ede055bc90a1a22f8d0819645471c6a7
fargito <fr.farge96@gmail.com>
2023-11-28 09:55:48 +0100

    chore(documentation): enable prod docs to point on staging

--- user-docs/documentation/hosting/utils/getDomainNames.ts
+++ user-docs/documentation/hosting/utils/getDomainNames.ts
@@ -8,6 +8,6 @@ export const getDomainNames = (stage: AppStage): string[] => {
       // while waiting for prod to be unblocked
       return ['staging.swarmion.dev', 'www.swarmion.dev'];
     case 'production':
-      return ['www.swarmion.dev'];
+      return [];
   }
 };
