15461543f6a61256549b06946fb1aee1f0fcd641
Guillaume Duboc <guillaumed@theodo.fr>
2023-11-29 22:04:34 +0100

    example(sls): bump react to v18

--- examples/sls/frontend/app/src/main.tsx
+++ examples/sls/frontend/app/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-ReactDOM.render(
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
 );
