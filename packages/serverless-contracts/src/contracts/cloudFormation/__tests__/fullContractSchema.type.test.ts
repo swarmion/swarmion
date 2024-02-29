7a5cda7faadf21c6a3ea2b9b0d26afd2da596b64
fargito <fr.farge96@gmail.com>
2024-02-29 11:02:52 +0100

    chore(prettier): format fix

--- packages/serverless-contracts/src/contracts/cloudFormation/__tests__/fullContractSchema.type.test.ts
+++ packages/serverless-contracts/src/contracts/cloudFormation/__tests__/fullContractSchema.type.test.ts
@@ -2,9 +2,8 @@ import { JSONSchema } from 'json-schema-to-ts';
 
 import { FullContractSchemaType } from '../types';
 
-type Check = FullContractSchemaType<'hello'> extends JSONSchema
-  ? 'pass'
-  : 'fail';
+type Check =
+  FullContractSchemaType<'hello'> extends JSONSchema ? 'pass' : 'fail';
 
 const check: Check = 'pass';
 check;
