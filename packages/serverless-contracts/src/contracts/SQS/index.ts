e662cd8e800c494f48c89f996229607de5eda1a8
CorentinDoue <corentin.doue.pro@gmail.com>
2024-04-12 08:19:23 +0200

    fix(serverless-contracts): add missing exports

--- packages/serverless-contracts/src/contracts/SQS/index.ts
+++ packages/serverless-contracts/src/contracts/SQS/index.ts
@@ -1,9 +1,3 @@
 export { SQSContract } from './sqsContract';
-export { getSQSHandler } from './features';
-export type {
-  SQSHandler,
-  SwarmionSQSHandler,
-  SwarmionLambdaSQSHandler,
-  GetSQSHandlerOptions,
-  SwarmionSQSRecord,
-} from './types';
+export { getSQSHandler, buildSendMessage, buildSendMessages } from './features';
+export * from './types';
