f65b7aa4e30b3163faface14c282df3762375fee
CorentinDoue <corentin.doue.pro@gmail.com>
2024-04-19 19:53:46 +0200

    feat(serverless-contracts): add explicit logs on parsing error

--- packages/serverless-contracts/src/contracts/SQS/utils/parseRecord.ts
+++ packages/serverless-contracts/src/contracts/SQS/utils/parseRecord.ts
@@ -7,17 +7,25 @@ export const parseRecord =
   <MessageBody, MessageAttributes>({
     bodyParser,
   }: GetSQSHandlerOptions<boolean>) =>
-  ({
-    body,
-    messageAttributes,
-    ...rest
-  }: SQSRecord): SwarmionSQSRecord<MessageBody, MessageAttributes> => ({
-    ...rest,
-    body:
-      bodyParser !== undefined
-        ? (bodyParser(body) as MessageBody) // Validation is done after
-        : (body as MessageBody), // Validation is done after
-    messageAttributes: parseMessageAttributes(
-      messageAttributes,
-    ) as MessageAttributes, // Validation is done after
-  });
+  (
+    { body, messageAttributes, ...rest }: SQSRecord,
+    index: number,
+  ): SwarmionSQSRecord<MessageBody, MessageAttributes> => {
+    try {
+      return {
+        ...rest,
+        body:
+          bodyParser !== undefined
+            ? (bodyParser(body) as MessageBody) // Validation is done after
+            : (body as MessageBody), // Validation is done after
+        messageAttributes: parseMessageAttributes(
+          messageAttributes,
+        ) as MessageAttributes, // Validation is done after
+      };
+    } catch (e) {
+      console.error(
+        `Error while parsing SQS Record at index ${index}. Please use the \`logRawEvent\` option to debug`,
+      );
+      throw e;
+    }
+  };
