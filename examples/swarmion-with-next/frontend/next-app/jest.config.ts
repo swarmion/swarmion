331bbb33a3fd2f76b31c59565ffc0c1beaee0718
Guillaume Duboc <guillaumeduboc2@gmail.com>
2024-12-02 09:04:54 +0100

    chore(swarmion-next): add explicit type for jest config

--- examples/swarmion-with-next/frontend/next-app/jest.config.ts
+++ examples/swarmion-with-next/frontend/next-app/jest.config.ts
@@ -1,3 +1,4 @@
+import type { Config } from '@jest/types';
 import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
@@ -12,7 +13,7 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setupAfterEnv.ts'],
-};
+} satisfies Config.InitialProjectOptions;
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(customJestConfig);
